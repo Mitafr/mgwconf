70ed9d685ff6e34cbc9143cdf1348848dbeb1eb6
MitaFR <martin.pelletier78@hotmail.com>
2024-07-13 17:16:41 +0200

    fmt

--- mgwconf-cli/src/command/get_api_client_credential.rs
+++ mgwconf-cli/src/command/get_api_client_credential.rs
@@ -10,14 +10,24 @@ pub struct GetAllApiClientCredential {}
 
 impl GetAllApiClientCredential {
     pub fn output_file() -> File {
-        OpenOptions::new().append(true).create(true).truncate(false).open("output/api_client_credentials").unwrap()
+        OpenOptions::new()
+            .append(true)
+            .create(true)
+            .truncate(false)
+            .open("output/api_client_credentials")
+            .unwrap()
     }
 }
 
 #[async_trait]
 impl CommandTrait for GetAllApiClientCredential {
     async fn execute(app: &CliApp) {
-        <CliApp as AppTrait<Config>>::dispatch(app, mgwconf_network::event::IoEvent::GetAllApiClientCredentials).await.unwrap();
+        <CliApp as AppTrait<Config>>::dispatch(
+            app,
+            mgwconf_network::event::IoEvent::GetAllApiClientCredentials,
+        )
+        .await
+        .unwrap();
     }
 
     fn num_op() -> usize {
